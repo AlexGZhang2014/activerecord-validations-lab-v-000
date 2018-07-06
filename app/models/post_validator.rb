e6892e99d99f05450aa2eba0c62a0db932828301
Alex Zhang <alexgzhang92@gmail.com>
2018-07-06 21:49:21 +0000

    automatically backed up by learn

--- app/models/post_validator.rb
+++ app/models/post_validator.rb
@@ -1,7 +1,7 @@
 class PostValidator < ActiveModel::Validator
   def validate(record)
-    unless record.name.include? "Won't Believe", "Secret", "Top[number]", "Guess"
-      record.errors[:name] << "Your title does not contain enough clickbait!"
+    unless record.title.include? "Won't Believe", "Secret", "Top[number]", "Guess"
+      record.errors[:title] << "Your title does not contain enough clickbait!"
     end
   end
 end
